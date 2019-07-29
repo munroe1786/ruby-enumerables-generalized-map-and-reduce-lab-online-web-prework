fb8dd3e3164ae2036d8be4e5bcbfc5f5ef8599aa
Eliza Munroe <era3munroe@gmail.com>
2019-07-29 12:06:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(array)
+    array.map{|n| n * -1}
+end
+
+def map(array)
+  array.map{|n| n}
+end
+
+def map(array)
+  array.map{|n| n * 2}
+end
+
+def map(array)
+  array.map{|n| n **2}
+end
+
+def reduce_to_total(source_array,starting_point)
+  source_array.reduce(100){|sum, element| sum + element}
+end
