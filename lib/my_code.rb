4919f4edfd8d0d5951b15a0b1b5529d8ab4d540d
munroe1786 <era3munroe@gmail.com>
2019-10-07 12:29:18 -0400

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+    #map returns a new array of manipulated elements
+    new_array = []
+    counter = 0
+    while counter < array.length
+        new_array << yield(array[counter])
+        counter += 1
+    end
+    new_array
+end 
+
+def reduce(array, starting_point = nil)
+    if starting_point 
+        sum = starting_point
+        counter = 0
+    else
+        sum = array[0]
+        counter = 1
+    end
+
+    while counter < array.length
+        sum = yield(sum, array[counter])
+        counter += 1
+    end
+    sum
+end
